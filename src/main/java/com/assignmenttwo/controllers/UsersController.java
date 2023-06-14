ac501f21a09f61de66a26ac4c546d09bf83e4d18
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-06-13 23:07:22 -0700

    Update UsersController.java

--- src/main/java/com/assignmenttwo/controllers/UsersController.java
+++ src/main/java/com/assignmenttwo/controllers/UsersController.java
@@ -10,9 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.assignmenttwo.models.User;
@@ -33,7 +37,7 @@ public class UsersController {
         model.addAttribute("us", users);
         return "users/showAll";
     }
-
+    
     @GetMapping("/users/view/{uid}")
     public String getUser(Model model, @PathVariable String uid){
         
@@ -74,7 +78,7 @@ public class UsersController {
 
 
 
-    @GetMapping("users/edit/{uid}")
+    @GetMapping("/users/edit/{uid}")
     public String showEditForm(Model model, @PathVariable String uid) {
         System.out.println("EDIT User " + uid);
         // Retrieve the user from the database based on the given uid
@@ -85,7 +89,7 @@ public class UsersController {
             return "error"; // You can create an error page or redirect to an appropriate view
         }
         model.addAttribute("user", user);
-        return "users/editUser";
+        return "users/edit";
     }
 
     @PostMapping("/users/edit/{uid}")
@@ -102,15 +106,24 @@ public class UsersController {
         String newName = updatedUser.get("name");
         String newPwd = updatedUser.get("password");
         int newSize = Integer.parseInt(updatedUser.get("size"));
+        int newHeight = Integer.parseInt(updatedUser.get("height"));
+        String newHairColor = updatedUser.get("hairColor");
+        double newGpa = Double.parseDouble(updatedUser.get("gpa"));
         user.setName(newName);
         user.setPassword(newPwd);
         user.setSize(newSize);
+        user.setHeight(newHeight);
+        user.setHairColor(newHairColor);
+        user.setGpa(newGpa);
         userRepo.save(user);
         response.setStatus(200);
         return "redirect:/users/view"; // Redirect to the view page after updating a user
     }
+    
 
+    
 
     
 
 }
+
