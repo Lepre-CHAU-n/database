ac15c3315b324ce7c64ba6444c6969609e8a7f0b
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-06-17 17:56:23 -0700

    Update UsersController.java

--- src/main/java/com/assignmenttwo/controllers/UsersController.java
+++ src/main/java/com/assignmenttwo/controllers/UsersController.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,13 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.assignmenttwo.models.User;
+
 
 import jakarta.servlet.http.HttpServletResponse;
 import com.assignmenttwo.models.UserRepository;
@@ -59,7 +54,6 @@ public class UsersController {
         int newHeight = Integer.parseInt(newuser.get("height"));
         String newHairColor = newuser.get("hairColor");
         double newGpa = Double.parseDouble(newuser.get("gpa"));
-        
         userRepo.save(new User(newName, newPwd, newSize, newHeight, newHairColor, newGpa));
         response.setStatus(201);
         return "users/addedUser";
@@ -76,8 +70,6 @@ public class UsersController {
 
 
 
-
-
     @GetMapping("/users/edit/{uid}")
     public String showEditForm(Model model, @PathVariable String uid) {
         System.out.println("EDIT User " + uid);
@@ -86,15 +78,15 @@ public class UsersController {
         User user = userRepo.findById(id).orElse(null);
         if (user == null) {
             // Handle the case when the user with the specified ID does not exist
-            return "error"; // You can create an error page or redirect to an appropriate vieww
+            return "error"; // You can create an error page or redirect to an appropriate view
         }
         model.addAttribute("user", user);
         return "users/edit";
     }
-
-    @PostMapping("/users/edit/{uid}")
-    public String editUser(@PathVariable String uid, @RequestParam Map<String, String> updatedUser, HttpServletResponse response) {
-        System.out.println("UPDATE User " + uid);
+    @PostMapping("/users/edit")
+    public String editUser(@RequestParam Map<String, String> updatedUser, HttpServletResponse response) {
+        String uid = updatedUser.get("uid");
+        System.out.println("UPDATE User "+ (uid));
         // Retrieve the user from the database based on the given uid
         int id = Integer.parseInt(uid);
         User user = userRepo.findById(id).orElse(null);
@@ -117,9 +109,9 @@ public class UsersController {
         user.setGpa(newGpa);
         userRepo.save(user);
         response.setStatus(200);
-        return "redirect:/users/view"; // Redirect to the view page after updating a user
+        return "redirect:view"; // Redirect to the view page after updating a user
     }
-    
+
 
     
 
